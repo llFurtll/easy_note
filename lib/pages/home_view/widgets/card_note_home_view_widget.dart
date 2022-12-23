951e96d9999a04414b269ab0a66dee83b942d00d
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2022-12-23 13:07:12 -0300

    Criado botão de ação de deletar na listagem de anotações

--- lib/pages/home_view/widgets/card_note_home_view_widget.dart
+++ lib/pages/home_view/widgets/card_note_home_view_widget.dart
@@ -1,5 +1,6 @@
 import 'package:compmanager/screen_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../../../core/widgets/spacer.dart';
 import '../controller/home_view_controller.dart';
@@ -33,35 +34,62 @@ class CardNoteHomeViewWidget extends ScreenWidget<HomeViewController, HomeViewIn
       borderRadius: BorderRadius.all(Radius.circular(10.0)),
     );
 
-    return InkWell(
-      onTap: () {},
-      customBorder: shape,
-      child: Card(
-        shape: shape,
-        clipBehavior: Clip.antiAlias,
-        elevation: 10.0,
-        child: Container( // IMAGEM DE FUNDO
-          height: 90.0,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage("https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"),
-              fit: BoxFit.cover
+    return Slidable(
+      endActionPane: ActionPane(
+        extentRatio: 0.25,
+        motion: const StretchMotion(),
+        children: [
+          Flexible(
+            child: Center(
+              child: InkWell(
+                borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+                onTap: () {},
+                child: Center(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      color: Colors.red
+                    ),
+                    width: 70,
+                    height: 70,
+                    child: const Icon(Icons.delete, color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          )
+        ],
+      ),
+      child: InkWell(
+        onTap: () {},
+        customBorder: shape,
+        child: Card(
+          shape: shape,
+          clipBehavior: Clip.antiAlias,
+          elevation: 10.0,
+          child: Container( // IMAGEM DE FUNDO
+            height: 90.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"),
+                fit: BoxFit.cover
+              )
+            ),
+            child: Container( // SOMBRA
+              color: Colors.white.withOpacity(0.5),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  spacer(10.0),
+                  _buildTitle(),
+                  spacer(10.0),
+                  _buildFooter()
+                ],
+              ),
             )
           ),
-          child: Container( // SOMBRA
-            color: Colors.white.withOpacity(0.5),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                spacer(10.0),
-                _buildTitle(),
-                spacer(10.0),
-                _buildFooter()
-              ],
-            ),
-          )
         ),
       ),
     );

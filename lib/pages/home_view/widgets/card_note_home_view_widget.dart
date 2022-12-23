09b7f8981d115213cffce48783901fc8c73fbde9
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2022-12-23 13:33:26 -0300

    Alteardo forma de exibição dos botões de ação

--- lib/pages/home_view/widgets/card_note_home_view_widget.dart
+++ lib/pages/home_view/widgets/card_note_home_view_widget.dart
@@ -30,45 +30,29 @@ class CardNoteHomeViewWidget extends ScreenWidget<HomeViewController, HomeViewIn
   }
 
   Widget _buildCard(BuildContext context) {
-    const shape = RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-    );
-
-    return Slidable(
-      endActionPane: ActionPane(
-        extentRatio: 0.25,
-        motion: const StretchMotion(),
-        children: [
-          Flexible(
-            child: Center(
-              child: InkWell(
-                borderRadius: const BorderRadius.all(Radius.circular(50.0)),
-                onTap: () {},
-                child: Center(
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                      color: Colors.red
-                    ),
-                    width: 70,
-                    height: 70,
-                    child: const Icon(Icons.delete, color: Colors.white),
-                  ),
-                ),
-              ),
-            )
-          )
-        ],
+    return Container(
+      height: 100.0,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10.0))
       ),
       child: InkWell(
         onTap: () {},
-        customBorder: shape,
         child: Card(
-          shape: shape,
           clipBehavior: Clip.antiAlias,
           elevation: 10.0,
-          child: Container( // IMAGEM DE FUNDO
-            height: 90.0,
+          child: Slidable(
+            endActionPane: ActionPane(
+              extentRatio: 0.2,
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  icon: Icons.delete,
+                  backgroundColor: Colors.red,
+                  onPressed: (BuildContext context) {}
+                )
+              ],
+            ),
+            child: Container( // IMAGEM DE FUNDO
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: NetworkImage("https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"),
@@ -90,6 +74,7 @@ class CardNoteHomeViewWidget extends ScreenWidget<HomeViewController, HomeViewIn
               ),
             )
           ),
+          )
         ),
       ),
     );

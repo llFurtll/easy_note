6e5fc2e7fe39335920241b9349af9d8c4ca205b5
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2023-01-31 13:25:26 -0300

    Refatorado metodo de salvar arquivo para não depender de plugins externos para isso

--- lib/core/utils/save_file.dart
+++ lib/core/utils/save_file.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<bool> saveFile(String pathFile, String path, String nomeFile) async {
@@ -12,9 +11,9 @@ Future<bool> saveFile(String pathFile, String path, String nomeFile) async {
       pathFinal.createSync(recursive: true);
     }
 
-    final file = XFile(pathFile);
+    final file = File(pathFile);
     String pathToSave = "${pathFinal.path}/$nomeFile";
-    file.saveTo(pathToSave);
+    file.copy(pathToSave);
 
     return true;
   } catch (_) {

0533cb7ed24ec42fc1849816cd0df360e6003ce5
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2023-03-09 13:36:29 -0300

    Começado implementação de compartilhamento com pdf

--- lib/features/anotacao/presentation/share/controller/share_controller.dart
+++ lib/features/anotacao/presentation/share/controller/share_controller.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'dart:ui' as ui;
 
 import 'package:compmanager/screen_controller.dart';
 import 'package:easy_note/core/utils/create_dir.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:vsc_quill_delta_to_html/vsc_quill_delta_to_html.dart';
 
 import '../../../../../core/adapters/share_file_easy_note.dart';
 import '../../../../../core/arguments/share_anotacao_arguments.dart';
@@ -40,7 +44,7 @@ class ShareController extends ScreenController {
       .then((_) async {
         switch (args.typeShare) {
           case TypeShare.pdf:
-            return "";
+            return _createPdf();
           default:
             return await _createImageShare();
         }
@@ -80,5 +84,51 @@ class ShareController extends ScreenController {
     } catch (_) {
       return "";
     }
+  }
+
+  Future<String> _createPdf() async {
+    String image = "";
+    if (args.showImage) {
+      try {
+        if (args.anotacao.imagemFundo!.contains("lib")) {
+          ByteData bytes = await rootBundle.load(args.anotacao.imagemFundo!);
+          image = base64Encode(MemoryImage(bytes.buffer.asUint8List()).bytes);
+        } else {
+          image = base64Encode(MemoryImage(
+            File(args.anotacao.imagemFundo!).readAsBytesSync()
+          ).bytes);
+        }
+      } catch (e) {
+        image = "";
+      }
+    }
+
+    final delta = <Map<String, dynamic>>[...jsonDecode(args.anotacao.observacao!)];
+    final converter = QuillDeltaToHtmlConverter(delta);
+    final html = """
+      <html>
+        <head>
+          <style>
+            .title {
+              color: black;
+              font-size: 25px;
+              font-weight: bold;
+            }
+          </style>
+        </head>
+        <body>
+          <span class="title">${args.anotacao.titulo}</span>
+          ${converter.convert()}
+        </body>
+      </html>
+    """;
+    final dir = await createDir("share/pdf");
+    final file = await FlutterHtmlToPdf.convertFromHtmlContent(
+      html,
+      dir,
+      "anotacao-${args.anotacao.id}"
+    );
+
+    return file.path;
   }
 }

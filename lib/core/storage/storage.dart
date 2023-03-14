4514f121c966cd875b50f445528b5918e8d4d427
Daniel Melonari <daniel.melonari@SV-NTB894.local>
2023-03-14 12:58:22 -0300

    Alterado versão do sqlite

--- lib/core/storage/storage.dart
+++ lib/core/storage/storage.dart
@@ -14,7 +14,7 @@ class StorageImpl extends Storage<Database> {
   Future<void> initStorage() async {
     await openDatabase(
       join(await getDatabasesPath(), "note.db"),
-      version: 6,
+      version: 3,
       onCreate: storageCreate,
       onUpgrade: storageUpdate
     );
